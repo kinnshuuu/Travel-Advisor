cc11e319c7db287a91d57b99e024d8b62007eb8b
Kinshu Jain <kinshujain0@gmail.com>
2022-12-07 21:45:43 +0530

    Created header, map, list, placeDetails components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./components/Header/Header"; 
+import Map from "./components/Map/Map";
+import List from "./components/List/List";   
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+const App = () => 
+{
+    return(
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default App;
